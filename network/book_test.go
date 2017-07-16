358697fe85ea1c7bf445d81f5b99f838c0e2d0b5
Sergii Shumakov <seshumak@microsoft.com>
2017-07-16 12:43:37 +0200

    Changed error checks to not check for specific error type.

--- network/book_test.go
+++ network/book_test.go
@@ -18,165 +18,165 @@
 package network
 
 import (
-    "testing"
-    "fmt"
-    "math/rand"
-    "github.com/stretchr/testify/assert"
+	"fmt"
+	"math/rand"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestAddSavesPeer(t *testing.T) {
-    book := NewSimpleBook()
-    addr := "192.168.4.52"
-    book.Add(addr)
+	book := NewSimpleBook()
+	addr := "192.168.4.52"
+	book.Add(addr)
 
-    entry, _ := book.Get()
+	entry, _ := book.Get()
 
-    assert.Equal(t, addr, entry, "Entry %s was not found in book", addr)
+	assert.Equal(t, addr, entry, "Entry %s was not found in book", addr)
 }
 
-func TestGetReturnsErrBookEmpty(t *testing.T) {
-    book := NewSimpleBook()
+func TestGetReturnsErr(t *testing.T) {
+	book := NewSimpleBook()
 
-    _, err := book.Get()
+	_, err := book.Get()
 
-    assert.Equal(t, errBookEmpty, err, "Get should return error in case book is empty")
+	assert.NotNil(t, err, "Get should return error in case book is empty")
 }
 
 func TestAddDoesNotSavePeerIfBlacklisted(t *testing.T) {
-    book := NewSimpleBook()
-    addr := "125.192.78.113"
+	book := NewSimpleBook()
+	addr := "125.192.78.113"
 
-    book.Blacklist(addr)
-    book.Add(addr)
+	book.Blacklist(addr)
+	book.Add(addr)
 
-    _, err := book.Get()
+	_, err := book.Get()
 
-    assert.Equal(t, errBookEmpty, err, "Add should not save address in case it is blacklisted")
+	assert.NotNil(t, err, "Add should not save address in case it is blacklisted")
 }
 
 func TestAddSavesPeerIfBlacklistedAndWhitelistedLater(t *testing.T) {
-    book := NewSimpleBook()
-    addr := "125.192.78.113"
+	book := NewSimpleBook()
+	addr := "125.192.78.113"
 
-    book.Blacklist(addr)
-    book.Whitelist(addr)
-    book.Add(addr)
+	book.Blacklist(addr)
+	book.Whitelist(addr)
+	book.Add(addr)
 
-    entry, _ := book.Get()
+	entry, _ := book.Get()
 
-    assert.Equal(t, addr, entry, "Address should be saved after whitelisting")
+	assert.Equal(t, addr, entry, "Address should be saved after whitelisting")
 }
 
 func TestGetReturnsAddressWithHighestScoreWhenOtherConnectionsDropped(t *testing.T) {
-    book := NewSimpleBook()
-    addr1 := "127.54.51.66"
-    addr2 := "120.55.58.86"
-    addr3 := "156.23.41.24"
+	book := NewSimpleBook()
+	addr1 := "127.54.51.66"
+	addr2 := "120.55.58.86"
+	addr3 := "156.23.41.24"
 
-    book.Add(addr1)
-    book.Add(addr2)
-    book.Add(addr3)
+	book.Add(addr1)
+	book.Add(addr2)
+	book.Add(addr3)
 
-    book.Connected(addr1)
-    book.Dropped(addr1)
+	book.Connected(addr1)
+	book.Dropped(addr1)
 
-    book.Connected(addr2)
-    book.Dropped(addr2)
-    book.Connected(addr2)
-    book.Dropped(addr2)
+	book.Connected(addr2)
+	book.Dropped(addr2)
+	book.Connected(addr2)
+	book.Dropped(addr2)
 
-    book.Connected(addr3)
-    book.Disconnected(addr3)
-    book.Connected(addr3)
-    book.Disconnected(addr3)
+	book.Connected(addr3)
+	book.Disconnected(addr3)
+	book.Connected(addr3)
+	book.Disconnected(addr3)
 
-    entry, _ := book.Get()
+	entry, _ := book.Get()
 
-    assert.Equal(t, addr3, entry, "Address %s with highest score is expected. Actual address %s", addr3, entry)
+	assert.Equal(t, addr3, entry, "Address %s with highest score is expected. Actual address %s", addr3, entry)
 }
 
 func TestGetReturnsAddressWithHighestScoreWhenOtherConnectionsFailed(t *testing.T) {
-    book := NewSimpleBook()
-    addr1 := "127.54.51.66"
-    addr2 := "120.55.58.86"
-    addr3 := "156.23.41.24"
+	book := NewSimpleBook()
+	addr1 := "127.54.51.66"
+	addr2 := "120.55.58.86"
+	addr3 := "156.23.41.24"
 
-    book.Add(addr1)
-    book.Add(addr2)
-    book.Add(addr3)
+	book.Add(addr1)
+	book.Add(addr2)
+	book.Add(addr3)
 
-    book.Failed(addr1)
-    book.Failed(addr1)
-    book.Failed(addr1)
+	book.Failed(addr1)
+	book.Failed(addr1)
+	book.Failed(addr1)
 
-    book.Failed(addr2)
-    book.Failed(addr2)
-    book.Failed(addr2)
+	book.Failed(addr2)
+	book.Failed(addr2)
+	book.Failed(addr2)
 
-    book.Connected(addr3)
-    book.Disconnected(addr3)
+	book.Connected(addr3)
+	book.Disconnected(addr3)
 
-    entry, _ := book.Get()
+	entry, _ := book.Get()
 
-    assert.Equal(t, addr3, entry, "Address %s with highest score is expected. Actual address %s", addr3, entry)
+	assert.Equal(t, addr3, entry, "Address %s with highest score is expected. Actual address %s", addr3, entry)
 }
 
 func TestSampleReturnsErrorIfNoPeersAdded(t *testing.T) {
-    book := NewSimpleBook()
+	book := NewSimpleBook()
 
-    _, err := book.Sample()
+	_, err := book.Sample()
 
-    assert.NotNil(t, err)
+	assert.NotNil(t, err)
 }
 
 func TestSampleReturnsErrorIfOnlyNonActivePeersAdded(t *testing.T) {
-    book := NewSimpleBook()
-    addr1 := "127.54.51.66"
-    addr2 := "120.55.58.86"
-    addr3 := "156.23.41.24"
+	book := NewSimpleBook()
+	addr1 := "127.54.51.66"
+	addr2 := "120.55.58.86"
+	addr3 := "156.23.41.24"
 
-    book.Add(addr1)
-    book.Add(addr2)
-    book.Add(addr3)
+	book.Add(addr1)
+	book.Add(addr2)
+	book.Add(addr3)
 
-    _, err := book.Sample()
+	_, err := book.Sample()
 
-    assert.NotNil(t, err)
+	assert.NotNil(t, err)
 }
 
 func TestSampleReturnsAddedPeers(t *testing.T) {
-    book := NewSimpleBook()
-    addrsLen := book.sampleSize
-    addrs := make([]string, 0, addrsLen)
-    for i := 0; i < addrsLen; i++ {
-        addr := randomAddr()
-        addrs = append(addrs, addr)
-        book.Add(addr)
-        book.Connected(addr)
-    }
+	book := NewSimpleBook()
+	addrsLen := book.sampleSize
+	addrs := make([]string, 0, addrsLen)
+	for i := 0; i < addrsLen; i++ {
+		addr := randomAddr()
+		addrs = append(addrs, addr)
+		book.Add(addr)
+		book.Connected(addr)
+	}
 
-    sample, _ := book.Sample()
+	sample, _ := book.Sample()
 
-    assert.Subset(t, addrs, sample, "Expected sample to be a subset of addrs")
+	assert.Subset(t, addrs, sample, "Expected sample to be a subset of addrs")
 }
 
 func TestSampleReturnsSubsetOfAddedPeers(t *testing.T) {
-    book := NewSimpleBook()
-    addrsLen := 50
-    addrs := make([]string, 0, addrsLen)
-    for i := 0; i < addrsLen; i++ {
-        addr := randomAddr()
-        addrs = append(addrs, addr)
-        book.Add(addr)
-        book.Connected(addr)
-    }
+	book := NewSimpleBook()
+	addrsLen := 50
+	addrs := make([]string, 0, addrsLen)
+	for i := 0; i < addrsLen; i++ {
+		addr := randomAddr()
+		addrs = append(addrs, addr)
+		book.Add(addr)
+		book.Connected(addr)
+	}
 
-    sample, _ := book.Sample()
+	sample, _ := book.Sample()
 
-    assert.Subset(t, addrs, sample, "Expected sample to be a subset of addrs")
+	assert.Subset(t, addrs, sample, "Expected sample to be a subset of addrs")
 }
 
-
-func randomAddr() (string) {
-    return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(150), rand.Intn(150), rand.Intn(150), rand.Intn(150))
+func randomAddr() string {
+	return fmt.Sprintf("%d.%d.%d.%d", rand.Intn(150), rand.Intn(150), rand.Intn(150), rand.Intn(150))
 }
