d7973411a76fc3e6b0e578acac82fbe6c07a8d68
Sergii Shumakov <seshumak@microsoft.com>
2018-01-20 15:55:14 +0100

    Allowed tool to sort imports.

--- network/processor_test.go
+++ network/processor_test.go
@@ -23,9 +23,8 @@ import (
 	"testing"
 	"time"
 
-	"github.com/stretchr/testify/assert"
-
 	"github.com/rs/zerolog"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 )
