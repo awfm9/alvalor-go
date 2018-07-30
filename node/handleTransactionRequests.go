d9a414f9c67ef177207b4a039ce904ac7c810c92
Sergii Shumakov <sshumakov@hotmail.com>
2018-07-30 22:23:23 +0200

    Moved transaction requests queue code into separate type.

--- node/handleTransactionRequests.go
+++ node/handleTransactionRequests.go
@@ -40,9 +40,9 @@ func handleTransactionRequests(log zerolog.Logger, wg *sync.WaitGroup, net Netwo
 		}
 
 		requestMessages := getRequestMessages(requestsStream)
-		outgoingRequests := transformToOutgoingRequests(requestMessages)
+		requestsQueue := newTransactionRequestsQueue(requestMessages)
 
-		for addr, hashes := range outgoingRequests {
+		for addr, hashes := range requestsQueue.getData() {
 			request := &Request{Hashes: hashes}
 			err := net.Send(addr, request)
 			if err != nil {
@@ -86,39 +86,4 @@ Loop:
 		}
 	}
 	return requestMessages
-}
-
-// tranforms map of [tx hashes -> peers addr which have it] to the map of [peer addr -> tx hashes to request]
-func transformToOutgoingRequests(requests map[types.Hash][]string) map[string][]types.Hash {
-	result := make(map[string][]types.Hash)
-
-	//map where key is an address and value is amount of planned hashes to send
-	outgoingTxReqCountMap := make(map[string]int)
-	for hash, addresses := range requests {
-		//find address which has least amount of tx hashes to request
-		minAddr := ""
-		minAddrCount := 0
-		for _, addr := range addresses {
-			if count, ok := outgoingTxReqCountMap[addr]; ok {
-				if minAddr == "" || count < minAddrCount {
-					minAddr = addr
-					minAddrCount = count
-				}
-				continue
-			}
-			//Found zero planned requests for this peer, therefore just selecting it
-			minAddr = addr
-			break
-		}
-
-		outgoingTxReqCountMap[minAddr] = minAddrCount + 1
-
-		if outgoing, ok := result[minAddr]; ok {
-			result[minAddr] = append(outgoing, hash)
-		} else {
-			result[minAddr] = []types.Hash{hash}
-		}
-
-	}
-	return result
 }
