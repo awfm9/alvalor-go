dd02773ae661909058f33c52c786c0728dce1187
Max Wolter <awishformore@gmail.com>
2018-11-15 13:48:41 +0100

    fixed up subscribers package

--- node/state/subscribers/stream.go
+++ node/state/subscribers/stream.go
@@ -15,7 +15,7 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with Alvalor.  If not, see <http://www.gnu.org/licenses/>.
 
-package subscribe
+package subscribers
 
 //
 // func handleStream(log zerolog.Logger, wg *sync.WaitGroup, stream <-chan interface{}, subscribers <-chan *subscriber) {
